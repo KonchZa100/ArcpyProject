d59c7a0d2dc24fa78253482f7d1fc8bb1951b309
KonchZa100 <radionigor11@gmail.com>
2019-05-21 02:04:42 +0300

    s10 SpatialReference errors was find

--- Lesson2/codeS10/s10.py
+++ Lesson2/codeS10/s10.py
@@ -1,10 +1,19 @@
 import arcpy
+import os
 
 arcpy.env.overwriteOutput = True
 
-targetFC=
-folderToExamine=
+targetFC=r'F:\Demidov\PythEveryone\PyCharmProject\Lesson2\CountyLines.shp'
+folderToExamine=r'F:\Demidov\PythEveryone\PyCharmProject\Lesson2\PractiseData\Result'
 
 targetSRName=arcpy.Describe(targetFC).SpatialReference.Name
 arcpy.env.workspace = folderToExamine
 listOfFCs = arcpy.ListFeatureClasses()
+
+for currentFC in listOfFCs:
+    currentFCSRName = arcpy.Describe(currentFC).SpatialReference.Name
+    if currentFCSRName != targetSRName:
+        outCS = arcpy.SpatialReference('NAD 1983 UTM Zone 10N')
+        rootName = currentFC[:-4] + "_projected.shp"
+        arcpy.Project_management(currentFC, rootName, outCS)
+        arcpy.AddMessage(rootName)
