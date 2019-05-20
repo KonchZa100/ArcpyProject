318d4c8cfbd34f3584e28eec0133a0a75d5f57c4
KonchZa100 <radionigor11@gmail.com>
2019-05-20 22:38:08 +0300

    s9 was done. Model is working fine, instrument added in ArcGis

--- Lesson1/Precip2008/scripts/PrecipitationZones.py
+++ Lesson1/Precip2008/scripts/PrecipitationZones.py
@@ -9,27 +9,27 @@
 # Import arcpy module
 import arcpy
 arcpy.env.overwriteOutput = True
-Precipitation = arcpy.GetParameterAsText(0)
-Nebraska2= arcpy.GetParameterAsText(1)
-NebrascaPrecipZones = arcpy.GetParameterAsText(2)
+Precip2008Readings = arcpy.GetParameterAsText(0)
+Nebraska__2_= arcpy.GetParameterAsText(1)
+RasterT_Reclass1_Clip = arcpy.GetParameterAsText(2)
 
 # Local variables:
-#Precip2008Readings = r"F:\\Demidov\\PythEveryone\\PyCharmProject\\Lesson1\\Precip2008\\Precip2008.gdb\\Precip2008Readings"
-idw_precipitation = "F:\\Demidov\\PythEveryone\\PyCharmProject\\Lesson1\\USA.gdb\\idw_precipitation"
-reclassIdw = "F:\\Demidov\\PythEveryone\\PyCharmProject\\Lesson1\\Precip2008\\Precip2008.gdb\\reclassIdw"
-RasterT_Reclass1 = "F:\\Demidov\\PythEveryone\\PyCharmProject\\Lesson1\\Precip2008\\Precip2008.gdb\\RasterT_Reclass1.shp"
-#Nebraska = r"F:\Demidov\PythEveryone\PyCharmProject\Lesson1\Precip2008\Precip2008.gdb\Nebraska"
-#NebraskaPrecipZones = "F:\\Demidov\\PythEveryone\\PyCharmProject\\Lesson1\\Precip2008\\Precip2008.gdb\\NebraskaPrecipZones"
+#Precip2008Readings = "F:\\Demidov\\PythEveryone\\PyCharmProject\\Lesson1\\Precip2008\\Precip2008.gdb\\Precip2008Readings"
+IdwRaster = "F:\\Demidov\\PythEveryone\\PyCharmProject\\Lesson1\\Precip2008\\IdwRaster"
+ReclassIdw = "F:\\Demidov\\PythEveryone\\PyCharmProject\\Lesson1\\Precip2008\\Precip2008.gdb\\ReclassIdw"
+RasterT_Reclass1 = "F:\\Demidov\\PythEveryone\\PyCharmProject\\Lesson1\\Precip2008\\Precip2008.gdb\\RasterT_Reclass1"
+#Nebraska__2_ = "F:\\Demidov\\PythEveryone\\PyCharmProject\\Lesson1\\Precip2008\\Precip2008.gdb\\Nebraska"
+#RasterT_Reclass1_Clip = "F:\\Demidov\\PythEveryone\\PyCharmProject\\Lesson1\\Precip2008\\Precip2008.gdb\\RasterT_Reclass1_Clip"
 
 # Process: IDW
-arcpy.gp.Idw_sa(Precipitation, "ID", idw_precipitation, "1850,46467", "2", "VARIABLE 12", "")
+arcpy.gp.Idw_sa(Precip2008Readings, "RASTERVALU", IdwRaster, "1850,46467", "2", "VARIABLE 12", "")
 
 # Process: Reclassify
-arcpy.gp.Reclassify_sa(idw_precipitation, "VALUE", "27715,960938 57484,319210 1;57484,319210 80383,056342 2;80383,056342 111132,789063 3", reclassIdw, "DATA")
+arcpy.gp.Reclassify_sa(IdwRaster, "VALUE", "27715,960938 57484,319210 1;57484,319210 80383,056342 2;80383,056342 111132,789063 3", ReclassIdw, "DATA")
 
 # Process: Raster to Polygon
-arcpy.RasterToPolygon_conversion(reclassIdw, RasterT_Reclass1, "SIMPLIFY", "VALUE")
+arcpy.RasterToPolygon_conversion(ReclassIdw, RasterT_Reclass1, "SIMPLIFY", "VALUE")
 
 # Process: Clip
-arcpy.Clip_analysis(RasterT_Reclass1, Nebraska2, NebrascaPrecipZones, "")
+arcpy.Clip_analysis(RasterT_Reclass1, Nebraska__2_, RasterT_Reclass1_Clip, "")
 
