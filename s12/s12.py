0970231052295efe34683f1984de9ffbc6f2f28b
KonchZa100 <radionigor11@gmail.com>
2019-05-24 00:47:29 +0300

    Code is working. Feature classes are added.

--- s12/s12.py
+++ s12/s12.py
@@ -1,17 +1,21 @@
 import arcpy
 
 arcpy.env.overwriteOutput = True
-poligon =r'F:\Demidov\PythEveryone\PyCharmProject\s12\CentralAmerica.shp'
-points =r'F:\Demidov\PythEveryone\PyCharmProject\s12\OSMpoints.shp'
-amenities = ('school','hospital','place_of_worship')
+Countries =r'F:\Demidov\PythEveryone\PyCharmProject\s12\CentralAmerica.shp'
+OSMpoints =r'F:\Demidov\PythEveryone\PyCharmProject\s12\OSMpoints.shp'
+amenity = ['school','hospital','place_of_worship']
 country = 'El Salvador'
 workspace= r'F:\Demidov\PythEveryone\PyCharmProject\s12'
 out_name = r'F:\Demidov\PythEveryone\PyCharmProject\s12\Geodatabase.gdb'
 
 arcpy.env.workspace = workspace
-arcpy.CreateFileGDB_management(workspace, out_name)
 
-arcpy.MakeFeatureLayer_management(points,'osm')
-arcpy.MakeFeatureLayer_management(poligon,'country', '"NAME" =' + "'" + country + "'")
+arcpy.MakeFeatureLayer_management(OSMpoints,'osm')
+arcpy.MakeFeatureLayer_management(Countries,'country', '"NAME" =' + "'" + country + "'")
 
 arcpy.SelectLayerByLocation_management('osm', 'WITHIN', 'country')
+for shel in amenity:
+    arcpy.MakeFeatureLayer_management('osm',shel+'1','"amenity"='+"' "+shel+"'")
+    #arcpy.FeatureClassToShapefile_conversion(str(shel), workspace)
+    arcpy.CopyFeatures_management(shel+'1', out_name + '\\' + str(shel))
+    arcpy.AddField_management(out_name + '\\' + str(shel),'source','TEXT')
